f0f2bfae7ffb88b97cc9bf5c4813eb1b875fb1a8
nurali-code <nur3.dav.97@gmail.com>
2024-03-29 20:03:23 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+$('.menu-bar, .close-menu-bar').on('click', function () {
+    $('.navbar-wrapper').toggleClass('active');
+})
+// $('.nav__link').on('click', function () {
+//     $('.nav, .btn_menu, body').removeClass('is_active');
+// })
+
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 5) { $('.header-area').addClass('is-fixed'); }
+    else { $('.header-area').removeClass('is-fixed'); }
+});
+
+// $('a[href*="#"]').on('click', function (e) {
+//     e.preventDefault();
+//     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 80, }, 300,)
+// });
